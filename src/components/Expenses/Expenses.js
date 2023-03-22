0ee4536c6f236cc563db92e8bc56d8dab7a7be92
a-basuony <basouny85@gmail.com>
2023-03-22 16:15:17 +0200

    maxmillian  React Course on udemy

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import ExpenseList from "./ExpenseList";
+import "./Expenses.css";
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpenseList items={filteredExpenses} />
+
+        {/* first option filter ===>  */}
+
+        {/* {filteredExpenses.length === 0 && <p> there is no expenses</p>}
+        {filteredExpenses.length > 0 &&
+          filteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))} */}
+
+        {/* second option filter ===> */}
+
+        {/* {filteredExpenses.length === 0 ? (
+          <p> there is no expenses</p>
+        ) : (
+          filteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))
+        )} */}
+
+        {/* third option filter ===>  down*/}
+
+        {/* {filteredExpenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))} */}
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
